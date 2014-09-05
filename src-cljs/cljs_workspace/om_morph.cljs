419d7e325181ac254a91b6a6ca3709d357f97bbc
Robin Schreiber <robin.schreiber@me.com>
2014-09-04 21:20:31 -0700

    Add onClick callback option for morphs.

--- src-cljs/cljs_workspace/om_morph.cljs
+++ src-cljs/cljs_workspace/om_morph.cljs
@@ -83,6 +83,7 @@
       (let [style (dict->js (extract-style (:morph app)))]
         (dom/div  #js {:style style
                      :className "morphNode"
+                     :onClick #(when-let [cb (get-in @app [:morph :onClick])] (cb %))
                      :onMouseDown #(draggable/start-dragging % app owner)
                      :onMouseUp #(draggable/stop-dragging % app)
                     } 
