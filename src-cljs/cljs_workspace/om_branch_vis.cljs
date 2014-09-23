11ee93a25cca51264ae0ca21265c6daec74bfb84
Robin Schreiber <robin.schreiber@me.com>
2014-09-22 19:15:19 -0700

    start implementing merge interface

--- src-cljs/cljs_workspace/om_branch_vis.cljs
+++ src-cljs/cljs_workspace/om_branch_vis.cljs
@@ -3,7 +3,8 @@
             [om.dom :as dom :include-macros true]
             [cljs-workspace.morph 
               :as morphic 
-              :refer [morph set-position set-fill set-extent add-morph remove-morph]]))
+              :refer [morph set-position set-fill set-extent add-morph remove-morph]]
+            [cljs-workspace.branch-merge :as branch-merge]))
 
 (enable-console-print!)
 ;; utilities to render a branching unrolled linked list
@@ -24,14 +25,25 @@
 ;
 ; Data structure:
 ;
-; {:id ... :data [ . . . .  {:cont ---------------------> [ . . . . . ] 
+; {:id ... :data -> [ . . . .  {:cont ---------------------> {:root . :data -> [ . . . . . ]} 
 ;                            :fork | }]}
 ;                                  \                        
 ;                                   V                      
-;                                  {:id ... :data [ . . . . . { . . }]}
+;                                  {:id ... :data -> [ . . . . . { . . }]}
 
 (def current-branch-ref (atom nil)) ;; supplied by the user of the module
 (def branch-callback-fn (atom (fn [branch] (prn "Clicked on me!"))))
+(def reverted-index (atom -1))
+(def rendered-tree (atom nil))
+
+(def highlighted-branches (atom {}))
+
+(def history-marker {:morph {:Position {:x 0 :y 1}}
+                     :shape {:Fill "lightgreen"
+                             :Extent {:x 5 :y 5}}})
+
+
+; TODO visualize the movement of one branch into the other
 
 (defn is-on-current [branch]
   (if @current-branch-ref
@@ -41,14 +53,37 @@
             (= @current-branch-ref (branch :root))))
     false))
 
+(defn get-coloring [branch]
+  (prn "HIGHLIGHTED: " @highlighted-branches)
+  (if (contains? @highlighted-branches (hash branch))
+    "red"
+    (if (is-on-current branch) "green" "black")))
+
 (defn scale-branch-model [model max-width] model)
+
+(defn highlight-branch [root path]
+  (when-let [next-step (first path)]
+    (let [branch-part (root next-step)]
+      (do 
+        (swap! highlighted-branches assoc (hash branch-part) true)
+        (if (and (= 1 (count path)) (= :cont next-step))
+          (let [fp? (last @(branch-part :data))]
+            (if (contains? fp? :cont)
+              (highlight-branch fp? [:cont])))))
+      (let [fp (last @(branch-part :data))]
+        (highlight-branch fp (rest path))))))
+
+(defn visualize-merge-candidates [fork-point path-to-a path-to-b]
+  (prn "highlighting: " path-to-a " " path-to-b)
+  (highlight-branch fork-point path-to-a)
+  (highlight-branch fork-point path-to-b)
+  (refresh-view))
 
 (defn render-stub [branch start-point]
   [{ ; id nessecary? not for now... we do not alter the morph structure but just rerender it completely
     :morph {:MorphClass "Path"
             :Position start-point
-            :onClick (fn [e] 
-                        (prn "switching via stub click to branch " (branch :id))
+            :onClick (fn [this] 
                         (@branch-callback-fn branch)
                         false)}
     :shape {:ShapeClass "Path"
@@ -58,37 +93,55 @@
             :submorphs []},
             20])
 
-(defn render-bubble [origin r fill]
-  {:morph {:Position origin}
+(defn render-bubble [origin r fill branch]
+  { :id (if (contains? branch :id) (branch :id) (get-in branch [:root :id]))
+    :morph {:Position origin
+           :onClick (fn [this]
+                      (branch-merge/select-for-merge 
+                        (if (contains? branch :id) branch (branch :root))
+                        (fn [a b]
+                          (let [[fp pa pb] (branch-merge/fork-point a b @rendered-tree)]
+                            (visualize-merge-candidates fp pa pb))))
+                      (let [fill (if (= (get-in this [:shape :Fill]) "red") 
+                                    (do
+                                      ; (remove-morph branch-view (this :id) (str "box-" (this :id)))
+                                      "green")
+                                    (let [textfield { :id (str "box-" (this :id))
+                                                      :morph {:Position {:x 15 :y -3} 
+                                                             :MorphClass "Text" 
+                                                             :TextString (str (this :id))}
+                                                      :shape {:ShapeClass "Text"}}]
+                                       (add-morph branch-view (this :id) textfield)
+                                       "red"))]
+                        (set-fill branch-view (this :id) fill))
+                      false)}
    :shape {:ShapeClass "Ellipse"
            :Extent {:x r :y r}
-           :Fill fill}})
+           :Fill fill}
+   :submorphs []})
 
 (defn render-leaf [cont start-point]
-  (let [fill (if (is-on-current cont) "green" "black")]
+  (let [fill (get-coloring cont)]
     [{ ; id nessecary? not for now... we do not alter the morph structure but just rerender it completely
       :morph {:MorphClass "Path"
               :Position start-point
-              :onClick (fn [e] 
+              :onClick (fn [this] 
                           (if (contains? cont :root)
                             (let [b (cont :root)]
-                              (prn "switching via leaf click to branch: " (b :id))
                               (@branch-callback-fn b))
                             (let [b cont]
-                              (prn "switching via stub leaf click to branch: " (b :id))
                               (@branch-callback-fn b)))
                           false)}
       :shape {:ShapeClass "Path"
               :StrokeWidth 5
               :Fill fill
-              :PathElements [{:x 0 :y 0} {:x (count @(cont :data)) :y 0}]}
-              :submorphs [(render-bubble {:x (count @(cont :data)) :y -1} 10 fill)]}, ; place a cute bubble at the end :-)
+              :PathElements [{:x 0 :y 0} {:x (count @(cont :data)) :y 0} ]}
+              :submorphs [(render-bubble {:x (count @(cont :data)) :y -1} 10 fill cont)]}, ; place a cute bubble at the end :-)
               20]))
 
 (defn smoothen [vertices]
   ; interpolate last 2 edges, 
   ; by substituting the last 2 points with an arc
-  (prn "smoothening: " vertices)
   (let [[middle end] (take-last 2 vertices)
         half-point (- (end :x) (middle :x))
         rounded-edge {:type :arc
@@ -107,8 +160,8 @@
             :PathElements (smoothen (map #(translat - % start) [start (assoc end :x (start :x)) end]))}})
 
 (defn render-fork [branch cont fork start-point]
-  (let [fill (if (is-on-current cont) "green" "black")
-        end-point {:x (count branch) :y 0} ; based on start-point and number of entries
+  (let [fill (get-coloring branch)
+        end-point {:x (count @(branch :data)) :y 0} ; based on start-point and number of entries
         [cont-path cont-space] (render-branch cont end-point) ; we have a fork, return a path that visualizes branching
         fork-start-point {:x (+ (end-point :x) 10) :y cont-space} ; based on end-point and space occupation of the cont
         [fork-path fork-space] (render-branch fork fork-start-point)
@@ -116,8 +169,7 @@
             [{; id nessecary? not for now... we do not alter the morph structure but just rerender it completely
              :morph {:MorphClass "Path"
                      :Position start-point
-                     :onClick (fn [e] 
-                                (prn "switching via fork click to branch " (get-in cont [:root :id]))
+                     :onClick (fn [this] 
                                 (@branch-callback-fn (cont :root))
                                 false)}
              :shape {:ShapeClass "Path"
@@ -127,38 +179,63 @@
              :submorphs [cont-path arc-path fork-path]},
               (+ cont-space fork-space)]))
 
+(defn attach-marker [branch-morph branch]
+  (let [marker (assoc-in history-marker [:morph :Position :x] @reverted-index)]
+    (update-in branch-morph [:submorphs] conj marker)))
+
 (defn render-branch
   ([branch]
-     ; initial call to first branch assume origin as starting point
-     (render-branch branch {:x 0 :y 0}))
+    (render-branch branch {:x 0 :y 0}))
   ([branch start-point]
      ; check if the current branch has any entries
-     (if-let [end (last @(branch :data))]
-        (if-let [cont (end :cont)] ; check for fork
-          (let [fork (end :fork)]
-            (render-fork @(branch :data) cont fork start-point))
-          (render-leaf branch start-point))
-        (render-stub branch start-point) ; this brunch has no entries yet
-      )
-  ))
+     (let [[bm sp] 
+       (if-let [end (last @(branch :data))]
+          (if-let [cont (end :cont)] ; check for fork
+            (let [fork (end :fork)]
+              (render-fork branch cont fork start-point))
+            (render-leaf branch start-point))
+          (render-stub branch start-point) ; this brunch has no entries yet
+        )]
+        [(if (and (contains? branch :id) ; we only attach markers to the root of the branch
+                  (is-on-current branch) 
+                  (> @reverted-index 0)) (attach-marker bm branch) bm) sp])))
 
-(defn render-tree [tree branch branch-callback]
+(defn refresh-view []
+  (let [[model _] (render-branch @rendered-tree)]
+      (swap! branch-view assoc :submorphs [merge-button (scale-branch-model model 400)])))
+
+(defn render-tree [tree branch branch-callback reverted]
   ; idea, walk to the leaves of the tree, and then start traversing
   ; from there and end at the root. This way we can make sure, that we
   ; reserve enough space such that all branches are neatly spaced apart)
   (when (not= @current-branch-ref branch) (prn "have to recolor!"))
+  (reset! rendered-tree tree)
   (reset! current-branch-ref branch)
   (reset! branch-callback-fn branch-callback)
-  (let [[model space] (render-branch tree)]
-      ; render stuff with om...
-      (prn container)
-      (swap! branch-view assoc :submorphs [(scale-branch-model model 400)])))
+  (reset! reverted-index reverted)
+  (let [[model _] (render-branch tree)]
+      (swap! branch-view assoc :submorphs [merge-button (scale-branch-model model 400)])))
+
+(def merge-button 
+  {:id "mergeButton"
+   :morph {:Position {:x 400 :y 100}
+           :onClick (fn [this]
+                      (reset! highlighted-branches {})
+                      (refresh-view))}
+                      ;(branch-merge/merge-staged-branches @rendered-tree))}
+   :shape {:Extent {:x 60 :y 15}
+           :BorderColor "darkgrey"
+           :Fill "lightgrey"}
+   :submorphs [{:morph {:MorphClass "Text"
+                        :TextString "Merge?"
+                        :Locked true}
+                :shape {:ShapeClass "Text"}}]})
 
 (def branch-view (atom
   {:id "branchView"
    :shape {:Extent {:x 400 :y 100}}
    :morph {:Position {:x 50 :y 50} :isDraggable true}
-   :submorphs []}))
+   :submorphs [merge-button]}))
 
 (om/root
   (fn [app owner]
