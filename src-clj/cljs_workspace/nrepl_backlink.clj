51712666cde6126f843e29dc09df3838662764e2
Robert Krahn <robert.krahn@gmail.com>
2014-09-23 23:45:43 -0700

    nrepl "backlink" server now correctly connects back to clojurescript environment using cemerick's piggieback

--- src-clj/cljs_workspace/nrepl_backlink.clj
+++ src-clj/cljs_workspace/nrepl_backlink.clj
@@ -1,18 +1,18 @@
 (ns cljs-workspace.nrepl-backlink
-; (ns user
   (:require [clojure.tools.nrepl.server :as server]
             [clojure.tools.nrepl :as nrepl]
-            [cider.nrepl]
-            [cemerick.piggieback]))
+            [cemerick.piggieback]
+            [cljs.repl.browser]))
 
 ; (defonce server (server/start-server :port 7888))
 
-(def default-env {:port 7888
-                  :client-port 9000
+(def default-env {:port 7889
+                  :client-port 9050
                   :server nil
                   :conn nil
                   :session nil
-                  :browser-repl-enabled false})
+                  :browser-repl-enabled false
+                  :browser-repl-env nil})
 
 (defn- reset-env
   [env]
@@ -33,7 +33,8 @@
                 :port port
                 :handler (apply server/default-handler
                                 #'cemerick.piggieback/wrap-cljs-repl
-                                (map resolve cider.nrepl/cider-middleware)))
+                                ; (map resolve cider.nrepl/cider-middleware)
+                                server/default-middlewares))
         conn (nrepl/connect :port port)
         session (nrepl/client-session (nrepl/client conn Long/MAX_VALUE))]
     (.setReuseAddress (:server-socket server) true)
@@ -50,16 +51,18 @@
   [env form]
   (when-not (running? env) (throw (Exception. "Server not running")))
   (let [{session :session} @env
-        result (doall (nrepl/message session {:op "eval" :code (str form)}))]
+        {browser-repl-env :browser-repl-env} @env
+        ; result (doall (nrepl/message session {:op "eval" :code (str form)}))
+        result (cemerick.piggieback/cljs-eval browser-repl-env form :verbose true)]
     result))
 
 (defn start-browser-repl-default
   [env]
-  (let [port (:client-port env)]
-   (cljs-eval env `(do (require 'cljs.repl.browser)
-                  (cemerick.piggieback/cljs-repl
-                   :repl-env (cljs.repl.browser/repl-env :port ~port)))))
-  (swap! env #(assoc % :browser-repl-enabled true)))
+  (let [port (:client-port @env)
+        browser-repl-env (cljs.repl.browser/repl-env :port port)]
+    (cemerick.piggieback/cljs-repl :repl-env browser-repl-env)
+    (swap! env #(assoc % :browser-repl-enabled true))
+    (swap! env #(assoc % :browser-repl-env browser-repl-env))))
 
 (defn start-browser-repl
   [env]
@@ -67,8 +70,9 @@
 
 (defn stop-browser-repl
   [env]
-  (cljs-eval env :cljs/quit)
-  (swap! env #(assoc % :browser-repl-enabled false)))
+  (when (:browser-repl-enabled env)
+    (cljs-eval env :cljs/quit)
+    (swap! env #(assoc % :browser-repl-enabled false))))
 
 
 (defn stop
@@ -98,13 +102,6 @@
 (defn start
   [& options]
   (apply start-with-env (atom default-env) options))
-
-(defonce repl-runtime (start))
-
-; -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
-
-(defn -main [& args]
-  (println "started"))
 
 (comment 
 
