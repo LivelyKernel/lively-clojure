89b90ae4cda7418854cd4d28463cabbbb2850f72
Robin Schreiber <robin.schreiber@me.com>
2015-05-22 11:33:06 +0000

    Extract Window Morph Code

--- src/cljs/cljs_workspace/makerspace.cljs
+++ src/cljs/cljs_workspace/makerspace.cljs
@@ -5,64 +5,13 @@
             [goog.style :as gstyle]
             [om.core :as om :include-macros true]
             [om.dom :as dom :include-macros true]
+            [cljs-workspace.morph.window :refer [create-window]]
             [cljs-workspace.morph :as morphic]
-            [cljs-workspace.repl :as repl])
+            [cljs-workspace.repl :as repl]
+            [rksm.cloxp-com.cloxp-client :as cloxp])
   (:import [goog.events EventType]))
 
-(def resize-button
-  {:id "Button"
-   :morph {:Position {:x 205 :y 155} 
-           :isDraggable true
-           :onDrag (fn [->self _] 
-                     (let [->owner (get @->self :owner)]
-                       (morphic/set-extent ->owner (get-in @->self [:morph :Position])))
-                     false)} ;; continue with usual drag handling
-   :shape {:Extent {:x 10 :y 10}
-           :BorderColor "orange"
-           :Fill "yellow"}})
-
-(def world-workspace
-  {:id "Workspace"
-   :morph {:Position {:x 100 :y 100}
-           :isDraggable true}
-   :shape {:Extent {:x 200 :y 150}
-           :BorderColor "grey"
-           :Fill "white"}})
-
-; interesting scenario: how do we attach behavior to a morph?
-(defn name-morph [name]
-  {:id "nameMorph"
-   :morph {:Position {:x 5 :y 10}
-           :MorphClass "Text"
-           :TextString name}
-   :shape {:Extent {:x 100 :y 20}}})
-
-(def min-box
-  {:id "minBox"
-   :morph {:Position {:x 0 :y 0}
-           :MorphClass "Text"
-           :TextString "X"}
-   :shpae {:Extent {:x 17 :y 17}
-           :Fill "red"
-           :BorderColor "brown"
-           :TextColor "white"}})
-
-(def close-box
-  {:id "closeBox"
-   :morph {:Position {:x 0 :y 0}
-           :MorphClass "Text"
-           :TextString "-"}
-   :shpae {:Extent {:x 17 :y 17}
-           :Fill "grey"
-           :BorderColor "darkgrey"}})
-
-(def window-bar
-  {:id "windowbar"
-   :morph {:Position {:x 0 :y 0}}
-   :shape {:Extent {:x 200 :y 30}
-           :BorderColor "grey"
-           :Fill "lightgrey"}
-   :submorphs [(name-morph "Workspace") min-box close-box]})
+(cloxp/start)
 
 (def space-state
   (atom {:id "World"
@@ -90,12 +39,11 @@
                  :BorderWidth 2
                  :Fill "lightgrey"}}})
 
+(def world-workspace (create-window {:position {:x 100 :y 100} :name "Workspace" :extent {:x 200 :y 300}}))
+
 (def space-cursor (morphic/make-cursor space-state))
 
 (defn $morph [id]
   (morphic/find-morph space-state id))
 
 (morphic/add-morph ($morph "World") world-workspace)
-(morphic/add-morph ($morph "Workspace") resize-button)
-(morphic/add-morph ($morph "Workspace") window-bar)
-(morphic/add-morph ($morph "Workspace") mario)
---
+++ src/cljs/cljs_workspace/morph/window.cljs
@@ -0,0 +1,92 @@
+(ns cljs-workspace.morph.window
+  (:require-macros [cljs.core.async.macros :refer [go]])
+  (:require [om.core :as om :include-macros true]
+            [om.dom :as dom :include-macros true]
+            [cljs-workspace.morph :as morphic])
+  (:import [goog.events EventType]))
+
+(def control-height 12)
+
+(defn name-morph [name]
+  {:id "nameMorph"
+   :morph {:Position {:x 50 :y 5}
+           :TextString name
+           :FontFamily "Arial"
+           :AllowInput false}
+   :shape {:ShapeClass "Text"
+           :Extent {:x 100 :y 20}}})
+
+(def close-box
+  {:id "closeBox"
+   :morph {:Position {:x 10 :y 10}
+           :onMouseEnter (fn [->self _] 
+                           (let [x-box (get-in ->self [:submorphs 0])]
+                             (morphic/toggle-visibility x-box)))
+           :onMouseLeave (fn [->self _] 
+                           (let [x-box (get-in ->self [:submorphs 0])]
+                             (morphic/toggle-visibility x-box)))
+           :onClick (fn [->self _]
+                      (let [->target (get-in ->self [:owner :owner])]
+                        (morphic/remove-morph (get-in ->target :owner) (get @->target :id))))}
+   :shape {:ShapeClass "Ellipse"
+           :Extent {:x control-height :y control-height}
+           :Fill "#ff6052"}
+   :submorphs [{:morph {:Position {:x 2 :y -2}
+                        :TextString "×"
+                        :FontSize 10 
+                        :AllowInput false
+                        :Visible false}
+                :shape {:ShapeClass "Text"}}]})
+
+(def min-box
+  {:id "minBox"
+   :morph {:Position {:x 30 :y 10}
+           :onMouseEnter (fn [->self _] 
+                           (let [x-box (get-in ->self [:submorphs 0])]
+                             (morphic/toggle-visibility x-box)))
+           :onMouseLeave (fn [->self _] 
+                           (let [x-box (get-in ->self [:submorphs 0])]
+                             (morphic/toggle-visibility x-box)))
+           :onClick (fn [->self]
+                      (let [->target (get-in ->self [:owner :owner])
+                            width (get @->target [:shape :Extent :x])]
+                        (morphic/set-extent ->target {:x width :y 15})))}
+   :shape {:ShapeClass "Ellipse"
+           :Extent {:x control-height :y control-height}
+           :Fill "#ffbe06"}
+   :submorphs [{:morph {:Position {:x 2 :y -2}
+                        :TextString "−"
+                        :FontSize 10 
+                        :AllowInput false
+                        :Visible false}
+                :shape {:ShapeClass "Text"}}]})
+
+(def window-bar
+  {:id "windowbar"
+   :morph {:Position {:x 0 :y 0}}
+   :shape {:Extent {:x 200 :y 30}}
+   :submorphs [(name-morph "Workspace") min-box close-box]})
+
+(defn resize-button [owner-extent]
+  {:id "Button"
+   :morph {:Position (morphic/add-points owner-extent {:x 5 :y 5}) 
+           :isDraggable true
+           :onDrag (fn [->self _]
+                     (let [->target (get-in ->self [:owner])]
+                       (morphic/set-extent ->target (morphic/add-points {:x -5 :y -5} (get-in @->self [:morph :Position]))) 
+                        false))} ;; continue with usual drag handling
+   :shape {:ShapeClass "Image"
+           :Extent {:x 15 :y 15}
+           :url "http://lively-web.org/core/media/halos/resize.svg"}})
+
+(defn create-window [{name :name, position :position, extent :extent}]
+  {:id name
+   :morph {:Position position
+           :isDraggable true}
+   :shape {:Extent extent
+           :BorderColor "grey"
+           :Fill "linear-gradient(to bottom, #f0f0f0, #e9e9e9)"
+           :BorderRadius 10
+           :BorderWidth 1
+           :DropShadow true}
+   :submorphs [(resize-button extent), window-bar]})
